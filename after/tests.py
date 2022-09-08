c23c837f95107a87c97b1ed59281287c2f0767a4
Cheukting <cheukting.ho@gmail.com>
2022-09-08 15:54:30 +0100

    fix typo

--- after/tests.py
+++ after/tests.py
@@ -2,9 +2,9 @@ import reminder as app
 from reminder import Task
 
 def test_find_task():
-    tast_list = [Task(name="pay rent"), Task(name="buy bread")]
-    assert app._find_task("buy bread", tast_list) == Task(name="buy bread")
+    task_list = [Task(name="pay rent"), Task(name="buy bread")]
+    assert app._find_task("buy bread", task_list) == Task(name="buy bread")
 
 def test_find_task_none():
-    tast_list = [Task(name="pay rent"), Task(name="buy bread")]
-    assert app._find_task("buy banana", tast_list) is None
+    task_list = [Task(name="pay rent"), Task(name="buy bread")]
+    assert app._find_task("buy banana", task_list) is None
