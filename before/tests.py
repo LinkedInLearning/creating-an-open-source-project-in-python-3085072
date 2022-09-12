008e85d8f8ac503004dfc7d30db552b4fe1fe814
Cheukting <cheukting.ho@gmail.com>
2022-09-12 22:15:38 +0100

    test exceptions

--- before/tests.py
+++ before/tests.py
@@ -1,16 +1,8 @@
 import reminder as app
 from reminder import Task
 
-import datetime as dt
-
 import pytest
 
-def test_to_date():
-    assert app._to_date("2022-09-01") == dt.date(2022, 9, 1)
-
-def test_to_date_exception():
-    with pytest.raises(ValueError, match="12345 is not in YYYY-MM-DD format."):
-        app._to_date("12345")
 
 @pytest.mark.parametrize("test_input, expected",
         [("buy bread", Task(name="buy bread")),
