04f4d58ae472a0305e61dcd46f55ddac2df779a4
Cheukting <cheukting.ho@gmail.com>
2023-01-22 16:50:30 +0000

    adding 03_03

--- before/tests.py
+++ before/tests.py
@@ -12,12 +12,20 @@ def test_to_date_exception():
     with pytest.raises(ValueError, match="12345 is not in YYYY-MM-DD format."):
         app._to_date("12345")
 
+@pytest.fixture
+def task_list():
+    return [Task(name="pay rent"), Task(name="buy bread")]
+
 @pytest.mark.parametrize("test_input, expected",
         [("buy bread", Task(name="buy bread")),
          ("buy banana", None),
          ("PAY RENT", Task(name="pay rent")),
          ])
-def test_find_task(test_input, expected):
-    task_list = [Task(name="pay rent"), Task(name="buy bread")]
+def test_find_task(test_input, expected, task_list):
     assert app._find_task(test_input, task_list) == expected
+
+def test_save_load_task_list(task_list):
+    app._save_task_list(task_list)
+    load_list = app._get_task_list()
+    assert task_list == load_list
 
