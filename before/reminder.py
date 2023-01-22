acfacd5d158f7f9c366871704accf71bc6fae9ec
Cheukting <cheukting.ho@gmail.com>
2023-01-22 17:51:30 +0000

    add 03_06

--- before/reminder.py
+++ before/reminder.py
@@ -5,11 +5,13 @@ from typing import Optional, List
 from difflib import SequenceMatcher as SM
 from dataclasses import dataclass
 
+
 @dataclass
 class Task:
     name: str = ""
     deadline: Optional[dt.date] = None
     done: bool = False
+
 
 def _get_task_list() -> List[Task]:
     try:
@@ -17,8 +19,10 @@ def _get_task_list() -> List[Task]:
     except Exception:
         return []
 
+
 def _save_task_list(task_list: List[Task]) -> None:
     pickle.dump(task_list, open("reminder.p", "wb"))
+
 
 def _overdue(deadline: Optional[dt.date]) -> bool:
     if deadline is None:
@@ -28,36 +32,41 @@ def _overdue(deadline: Optional[dt.date]) -> bool:
     else:
         return False
 
+
 def _to_date(deadline: str) -> dt.date:
     try:
         return dt.date.fromisoformat(deadline)
     except ValueError:
         raise ValueError(f"{deadline} is not in YYYY-MM-DD format.") from None
 
+
 def _find_task(target: str, task_list: List[Task]) -> Optional[Task]:
     for task in task_list:
         if target.lower() == task.name.lower():
             return task
 
-def _find_match(target: str):
+
+def _find_match(target: str, task_list: List[Task]):
     potential_match = []
     for task in task_list:
         score = SM(None, target, task.name).ratio()
         if score >= 0.9:
             potential_match.append((score, task.name))
     if potential_match:
-        potential_match = sorted(potential_match, key=lambda x:x[0], reverse=True)
+        potential_match = sorted(potential_match, key=lambda x: x[0], reverse=True)
         click.echo(f"Cannot find {target}, here are the close matches:")
         for num, match in enumerate(potential_match):
             click.echo(f"{num+1}. {match[1]}")
+
 
 @click.group()
 def app():
     pass
 
+
 @click.command()
 @click.option("--deadline", default=None, help="Enter date in YYYY-MM-DD format.")
-@click.argument('task')
+@click.argument("task")
 def add(task: str, deadline: str):
     """Add a task in reminders."""
     task_list = _get_task_list()
@@ -71,6 +80,7 @@ def add(task: str, deadline: str):
         task_list.append(Task(task, _to_date(deadline)))
     _save_task_list(task_list)
 
+
 @click.command()
 def list():
     """List all the task in reminders."""
@@ -83,6 +93,7 @@ def list():
         else:
             click.echo(f"{num+1}. {task.name}")
 
+
 @click.command()
 @click.argument("task")
 def remove(task: str):
@@ -92,8 +103,9 @@ def remove(task: str):
     if target is not None:
         task_list.remove(target)
     else:
-        _find_match(task)
+        _find_match(task, task_list)
     _save_task_list(task_list)
+
 
 @click.command()
 @click.argument("task")
@@ -107,10 +119,11 @@ def done(task: str):
         _find_match(task)
     _save_task_list(task_list)
 
+
 app.add_command(add)
 app.add_command(list)
 app.add_command(remove)
 app.add_command(done)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app()
